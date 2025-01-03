31ca95b852c608fb41f4150ecb567b2f2b2efff4
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-01-04 04:57:23 +0600

    Problem 2 solved

--- Array/Problem02.cpp
+++ Array/Problem02.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int LargeNumber(int arr[], int n){
+    int larg = arr[0];
+    for(int i= 1; i<n; i++){
+        if(arr[i] > larg){
+            larg = arr[i];
+        }
+    }
+
+    return larg;
+}
 int main(){
-    cout<<"Hello";
+    int array []= {1, 4, 5, 2, 9,-8, 7};
+    int size = sizeof(array)/ sizeof(array[0]);
+
+    cout<<"Largest number is: "<< LargeNumber(array, size);
     return 0;
 }
