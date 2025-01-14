bd5916c34676e663f256f949eed51f8deb497fb6
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-01-15 00:15:42 +0600

    Solve Roman to Interger Convertion problem

--- Basic CPP/Palindrome.cpp
+++ Basic CPP/Palindrome.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers and numbers ending in 0 (except 0 itself) are not palindromes
+        
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
